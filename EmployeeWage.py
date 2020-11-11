f5fd6ba8a0874ed76d6a19b110598c658cab9f45
b115030 <iitiangokul@gmail.com>
2020-11-11 22:11:09 +0530

    ADDED: functionality and test cases to check attendance

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -2,3 +2,10 @@ import random
 def print_welcome():
     print("Welcome to Employee Wage Computation Problem")
     return True
+def check_attendance():
+    random_attendance = round(random.random())
+    if random_attendance == 1:
+        print("Employee is present")
+        return True
+    print("Employee is absent")
+    return False
--- test_EmployeeWage.py
+++ test_EmployeeWage.py
@@ -4,3 +4,7 @@ class Test_print_welcome(TestCase):
     def test_print_welcome(self):
         from EmployeeWage import print_welcome
         self.assertTrue(print_welcome)
+
+    def test_check_attendance(self):
+        from EmployeeWage import check_attendance
+        self.assertTrue(check_attendance)
